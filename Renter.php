ba42f5a4cb3701b95fc1bc7613473d233f53f921
Josiah Liebert <114367489+Mitokongdrya@users.noreply.github.com>
2024-12-08 22:04:46 -0800

    Add Renters base file

---
+++ Renter.php
@@ -0,0 +1,100 @@
+<?php
+	session_start();
+	//$currentpage="View Employees"; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Property DB</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+	<style type="text/css">
+        .wrapper{
+            width: 70%;
+            margin:0 auto;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+		 $('.selectpicker').selectpicker();
+    </script>
+</head>
+<body>
+    <?php
+        // Include config file
+        require_once "config.php";
+//		include "header.php";
+	?>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+		    <div class="page-header clearfix">
+		     <h2> Property Manangement Database (CS 340) </h2> 
+                       <p> Project should include CRUD operations. In this website you can:
+				<ol> 	<li> CREATE new properties and leases </li>
+					<li> RETRIEVE all property managers and owners for a property and all renters and payments for a lease</li>
+                                        <li> UPDATE renter records</li>
+					<li> DELETE property manager, owner, and renter records </li>
+				</ol>
+		       <h2 class="pull-left">Renter Details</h2>
+            
+               <a href="createRenter.php" class="btn btn-success pull-right">Add New Renter</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+                    
+                    $sql = "SELECT * FROM Renter";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                            echo "<thead>";
+                                echo "<tr>";
+                                    echo "<th width=10%>Renter Id</th>";
+                                    echo "<th width=10%>First Name</th>";                                        
+                                    echo "<th width = 20%>Last Name</th>";
+                                    echo "<th width = 20%>Phone Number</th>";
+                                    echo "<th width = 15%>Lease Id</th>";
+                                    echo "<th width=10%>Action</th>";
+                                echo "</tr>";
+                            echo "</thead>";
+                            echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['RenterId'] . "</td>";
+                                        echo "<td>" . $row['Renter_FName'] . "</td>";
+                                        echo "<td>" . $row['Renter_LName'] . "</td>";
+                                        echo "<td>" . $row['Renter_Phone'] . "</td>";
+                                        echo "<td>" . $row['lid'] . "</td>";
+                                        echo "<td>";
+                                            echo "<a href='viewOwnersandManagers.php?RenterId=". $row['RenterId']./*"&Lname=".$row['Lname'].*/"' title='View Owners & Managers' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                            echo "<a href='updateEmployee.php?Ssn=". $row['Ssn'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            echo "<a href='deleteEmployee.php?Ssn=". $row['Ssn'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+											echo "<a href='viewDependents.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Dependents' data-toggle='tooltip'><span class='glyphicon glyphicon-user'></span></a>";
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. <br>" . mysqli_error($link);
+                    }
