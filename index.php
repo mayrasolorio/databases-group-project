d34dcc91b9f222a3403ee7278562869c045722e8
David Gesl <davidgesl123@gmail.com>
2024-12-10 18:01:37 -0800

    added button for manager and property overview table

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
         <a href="PropertyAndLease.php" class="btn btn-primary">View Properties & Lease</a>
         <a href="OwnerAndManager.php" class="btn btn-success">View Owners & Managers</a>
         <a href="Renter.php" class="btn btn-info">View Renters & Make Payments</a>
+        <a href="getManagerAndPropOverview.php" class="btn btn-warning pull-right">View Properties with Manager Details</a>
     </div>
 </body>
 </html>
